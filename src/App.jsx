9787b63732f52b5742f4d58012756ae697cb700f
danielbxs <danielbrilhantebs@gmail.com>
2025-12-30 17:15:47 -0300

    Change BrowserRouter usage to createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router";
 import HomePage from "./pages/HomePage";
 import CollectionPage from "./pages/CollectionPage";
 import UserPage from "./pages/UserPage";
@@ -7,26 +13,24 @@ import SignUpPage from "./pages/SignUpPage";
 import Layout from "./pages/Layout";
 import UserCollectionPage from "./pages/UserCollectionPage";
 
+const router = createBrowserRouter([
+  {
+    Component: Layout,
+    children: [
+      { path: "/", Component: HomePage },
+      { path: "/collection", Component: CollectionPage },
+      {
+        path: "/user",
+        Component: UserPage,
+        children: [{ path: "collection", Component: UserCollectionPage }],
+      },
+    ],
+  },
+
+  { path: "/signin", Component: SignInPage },
+  { path: "/signup", Component: SignUpPage },
+]);
+
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          {/* Card-related */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/collection" element={<CollectionPage />} />
-
-          {/* User area */}
-          <Route path="/user">
-            <Route index element={<UserPage />} />
-            <Route path="collection" element={<UserCollectionPage />} />
-          </Route>
-
-          {/* Auth */}
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
